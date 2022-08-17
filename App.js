cfac3f2e65306383de70baeccd7747e250e5ceaf
napajun <napajun2545@gmail.com>
2022-08-17 11:25:06 +0700

    17-08-2022
    17-08-2022

--- App.js
+++ App.js
@@ -1,52 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { Text, View, Button, TextInput } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-
-function Homescreen({ navigation, route }) {
-  React.useEffect(() => {
-    if (route.params?.post) {
-      //Post updated,do something with 'route.params.post'
-      //For Example, sent the post to the server
-    }
-  }, [route.params?.post]);
-
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Create Post"
-        onPress={() => navigation.navigate("CreatePost")}
-      />
-      <Text style={{ margin: 10 }}>Post :{route.params?.post} </Text>
-    </View>
-  );
-}
-
-function Createpostscreen({ navigation, route }) {
-  const [postText, setPostText] = React.useState("");
-
-  return (
-    // use Fragment
-    <>
-      <TextInput
-        multiline
-        placeholder="Please text here"
-        style={{ height: 200, padding: 10, backgroundColor: "white" }}
-        onChangeText={setPostText}
-        value={postText}
-      />
-      <Button
-        title="Click"
-        onPress={() => {
-          //Pass params bact to HomeScreen funsion
-          navigation.navigate("Home", { post: postText });
-        }}
-      />
-    </>
-  );
-}
+import FirstPage from "./Screens/FirstPage";
+import SecondPage from "./Screens/SecondPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -54,15 +13,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="FirstPage"
         screenOptions={{
-          headerStyle: { backgroundColor: "#93220A" },
-          headerTintColor: "azure",
-          headerTitleStyle: { fintWeight: "bold", fontSize: 30 },
+          headerStyle: { backgroundColor: "#198C86" },
+          headerTintColor: "#ffff",
+          headerTitleStyle: { fontWeight: "bold" },
         }}
       >
-        <Stack.Screen name="Home" component={Homescreen} />
-        <Stack.Screen name="CreatePost" component={Createpostscreen} />
+        <Stack.Screen name="FirstPage" component={FirstPage} />
+        <Stack.Screen name="SecondPage" component={SecondPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
